4c456f3256c976d2baff4195c4451c28f21deddc
Jethro <jethrotiago@hotmail.com>
2022-11-18 22:25:59 -0300

    Adicionando códigos constructot

---
+++ js/app/models/Negociacao.js
@@ -0,0 +1,13 @@
+class Negociacao {
+
+    constructor() {
+
+        this.data = new Date();
+        this.quantidade = 1;
+        this.valor = 0.0;
+
+    }
+
+};
+
+//toda classe que tem um construtor, só pode ser envocada com um new
