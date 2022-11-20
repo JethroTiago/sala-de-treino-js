d323f5f38cb26cd0c46182f3acd8c34fa5a6b78b
Jethro <jethrotiago@hotmail.com>
2022-11-19 22:05:20 -0300

    Criando o método obtemVolume

--- js/app/models/Negociacao.js
+++ js/app/models/Negociacao.js
@@ -8,8 +8,13 @@ class Negociacao {
 
     }
 
+    obtemVolume() {
+        return this.quantidade * this.valor;
+    }
+
 };
 
 //toda classe que tem um construtor, só pode ser envocada com um new
 //o new torna o this correspondente ao objeto criado
 //a função acima só será chmada se houver o new
+//se uma função é criada dentro de uma classe, ela é chamada de MÉTODO
