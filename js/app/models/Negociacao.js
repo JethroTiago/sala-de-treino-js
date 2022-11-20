3218fc0479cd3efd53b17d0b38bf41092270f847
Jethro <jethrotiago@hotmail.com>
2022-11-19 21:53:23 -0300

    Adicionando parametros ao constructor

--- js/app/models/Negociacao.js
+++ js/app/models/Negociacao.js
@@ -11,3 +11,5 @@ class Negociacao {
 };
 
 //toda classe que tem um construtor, só pode ser envocada com um new
+//o new torna o this correspondente ao objeto criado
+//a função acima só será chmada se houver o new
